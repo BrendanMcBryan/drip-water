1e53c82e0bb1993a73887769f4060944ddd9e7fd
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-04-19 08:27:43 -0700

    Style Button Adjustment

--- src/ui/ButtonIcon.jsx
+++ src/ui/ButtonIcon.jsx
@@ -2,19 +2,23 @@ import styled from 'styled-components';
 
 const ButtonIcon = styled.button`
   background: none;
+  background-color: var(--color-grey-200);
   border: none;
   padding: 0.6rem;
   border-radius: var(--border-radius-sm);
   transition: all 0.2s;
 
   &:hover {
-    background-color: var(--color-grey-100);
+    background-color: var(--color-blue-100);
   }
 
   & svg {
-    width: 2.2rem;
-    height: 2.2rem;
+    width: 1.5rem;
+    height: 1.5rem;
     color: var(--color-brand-600);
+    &:hover {
+      color: white;
+    }
   }
 `;
 
